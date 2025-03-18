a3b879faaf7af29ec37ca14adf682851a74e19c4
Djuybu <macminhduy2004@gmail.com>
2025-03-18 16:24:06 +0700

    k8s initialization

---
+++ src/test/java/org/example/kubernatesspring/KubernatesSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.kubernatesspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KubernatesSpringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
